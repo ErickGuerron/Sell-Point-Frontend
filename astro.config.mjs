a7cf4c70336ffb4722c5476e757838d243b098b2
ErickGuerron <erickguerron@yahoo.com>
2026-04-25 17:13:32 -0500

    feat: primera vercion de landing page

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,13 @@ import { defineConfig } from 'astro/config';
 
 import analogjsangular from '@analogjs/astro-angular';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [analogjsangular()]
+  integrations: [analogjsangular()],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
